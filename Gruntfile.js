16f0ce8d66d349c526ed3c673b3af693f4accf77
jenniferhuii <jennifer.hui@wayne.edu>
2017-10-16 16:12:30 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            width: 1110,
+            height: 600,
+            suffix: '_large_2x',
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
